c7b777dcbca1d46a18723b13791eddf33318703e
HenriqueHS19 <henriquesantos.hs34@gmail.com>
2020-05-20 20:51:58 -0300

    creation of Input and Button components

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logo from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = function() {
+    return(
+        <Container>
+            <Image source = { logo } />
+            <Title> Faça seu logon </Title>
+
+            <Input name = "email" icon = "mail" placeholder = "E-mail" />
+            <Input name = "password" icon = "lock" placeholder = "Senha" />
+
+            <Button> Entrar </Button>
+        </Container>
+    );
+}
+
+export default SignIn;
